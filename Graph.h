855c9bd7efc1b9abfae6432b98e2b7d564502872
mariapasaylo <mnapasaylo@gmail.com>
2023-11-20 15:32:33 -0500

    Update method headers for graph class

--- Graph.h
+++ Graph.h
@@ -1,27 +1,32 @@
-//
-// Created by Brianna Yanqui on 11/20/23.
-//
-
 #ifndef P3_GRAPH_H
 #define P3_GRAPH_H
 
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <utility>
+
+using namespace std;
 
 class Graph {
-    struct Edge
-            string end							destination
-    double weight						distance
+private:
+    struct Edge {
+        string end;     //this target destination
+        double weight;	//distance
+    };
 
-            vector<string> path 						cities in shortest path
-            u_map<string, vector<Edge>> adjList			key: city-date; value: vector of Edges to neighbors
+    vector<string> path; 						 //cities in shortest path
+    unordered_map<string, vector<Edge>> adjList; //key: city-date; value: vector of Edges to neighbors
 
-    double euclideanDist(pair<double>)
-    addVertex(string city)					add each key to map if it doesn’t already exist
-    addEdge(string end, weight)					if dist<threshold, add Edge to adjList[start]
+    double euclideanDist(double latitude, double longitude);      //calculates the edge weight given the long and lat
+    void addVertex(string city);					    //add each key to map if it doesn’t already exist
+    void addEdge(string end, double weight);			//if dist<threshold, add Edge to adjList[start]
 
-            void create(vector<string>)					for each Sighting in v, getCity(), getData()
-    double dijkstra(string start string end)		add shortest path keys to class member path
-    double modifiedBFS(string start, string end)	add shortest path keys to class member path
-    getPath()								get cities in shortest path
+public:
+    void create(vector<string>);					//for each Sighting in v, getCity(), getData()
+    double dijkstra(string start, string end);		//add shortest path keys to class member path
+    double modifiedBFS(string start, string end);	//add shortest path keys to class member path
+    vector<string> getPath(); 						//get cities in shortest path
 };
 
 
