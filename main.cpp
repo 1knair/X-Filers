b2c84f15653200d6a20a36ec048ef0d108834d15
Karthik Nair <karthiknair@ufl.edu>
2023-11-20 15:22:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <UFOSightings.h>
+#include <Graph.h>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Test" << std::endl;
+new UFOSightings()
+	UFOSightings.parseFile("ufo_sighting_data.csv");
+	new Graph graph;
+    string start, target; //Keys
+	
+    graph.create(UFOSightings.getCityDates());
+    
+    //UI stuff for input selection
+    cin >> start; //Get start key
+    cin >> target; //Get end key
+    //If we want to input weight, do it here, pass to graph
+
+	startCity = getCity(start)
+    targetCity = getCity(target); //Get city data from keys
+
+    //UI stuff for algo selection
+    Graph.dijkstra(startCity, targetCity);
+    Graph.modifiedBFS(startCity, targetCity);
+
+    //UI stuff to display string
+    for (const string& city : graph.getPath()) {
+        cout << city << " ";
+    }
+
     return 0;
+
 }
