f76961e976b7e94e3b3964b808a174caae3a4ed3
bluebird-ot <64104983+bluebird-ot@users.noreply.github.com>
2023-12-05 01:31:24 -0500

    uploading final changes

--- Graph.h
+++ Graph.h
@@ -6,83 +6,279 @@
 #include <unordered_map>
 #include <utility>
 #include <cmath>
+#include <queue>
+#include <tuple>
+#include <algorithm>
+#include "UFOSightings.h"
+#define X_PI 3.14159265358979323846
+#include <limits>
+
 
 using namespace std;
 
-class Graph {
-private:
-    struct Edge {
-        string target_destination;
-        long double weight;	//distance
-    };
+struct Edge{
+    string to; //city-state adjacent/neighbor node
+    long double weight;	//distances from sighting to neighboring sighting
+    pair<long double, long double> latLong; //latitude and longitude coordinates for testing
 
-    vector<string> path; 						 //cities in shortest path
-    unordered_map<string, vector<Edge>> adjList; //key: city-date; value: vector of Edges to neighbors
+    //parametrized constructor
+    Edge(string _to, long double _weight, pair<long double, long double> _latLong) : to(_to), weight(_weight), latLong(_latLong) {}
+};
+
+class Graph {
+//private:
+public:
+
+
+    //key is the city-state from node, value is a tuple
+    //first is the to nodes along with edge weights
+    //next is to keep track of cumulative distances for the dijkstra algorithm
+    //bool for keeping track if it has been visited
+    //MARIA REMOVE BOOLEAN AND LONG DOUBLE FOR DISTANCE?
+    unordered_map<string, tuple<vector<Edge>, long double, bool>> adjList;
+    int numVertices = 0; //MARIA DO WE EVER USE THIS
+    float threshold = 500; //distance in km to make an edge between two sightings
 
     /**
      * Calculates the edge weight (which is the distance) between the two nodes' latitude
      * and longitude using the Haversine formula.
-     * Source(https://www.geeksforgeeks.org/program-distance-two-points-earth/)
+     * Source(https://www.geeksforgeeks.org/program-distancesance-two-points-earth/)
      * @param start longitude and latitude of the starting node
      * @param end longitude and latitude of the ending node
      * @return double that corresponds to the edge weight
      */
     long double calculateEdgeWeight(pair<long double, long double> start, pair<long double, long double> end);
-    void addVertex(string key);					    //add each key to map if it doesn’t already exist
-    void addEdge(string start, string end);	    //if dist<threshold, add Edge to adjList[start]
 
-public:
-    void create(vector<string> v);					//for each Sighting in v, getCity(), getData()
-    double dijkstra(string start, string end);		//add shortest path keys to class member path
-    double modifiedBFS(string start, string end);	//add shortest path keys to class member path
-    vector<string> getPath(); 						//get cities in shortest path
+    /**
+     * Adds the key to the graph where the key represents
+     * the location of the UFO sighting.
+     * @param key is a string that represents city-state
+     */
+    void addVertex(const string& key);
+
+    /**
+     * Adds an edge between two UFO sightings if the weight
+     * is below the threshold. This means that the graph
+     * could be disconnected.
+     * @param from is the from city-state
+     * @param to is the to city-state
+     * @param weight is the distance between the from node and to node
+     * @param latLong is the latitude and longitude of from
+     */
+    void addEdge(const string& from, const string& to, long double weight, pair<long double, long double> latLong);
+
+    /**
+     * Creates the graph which consists of sightings as vertices
+     * and the edges being the distance in km between the nodes
+     * @param sightings contains the sighting objects read from a csv file
+     */
+    void create(UFOSightings& sightings);
+
+    /**
+     * Finds the shortest paths using the Dijkstra algorithm.
+     * Note that it calculates paths to all nodes, but we only return
+     * th path from the source node to target node. This takes into account
+     * the edge weights.
+     * Source(https://www.geeksforgeeks.org/printing-paths-dijkstras-shortest-path-algorithm/)
+     * @param start the source node
+     * @param end the target node
+     * @return vector of strings of city-state to get from start to end
+     */
+    vector<string> dijkstra(const string& start, const string& end);
+
+    /**
+     * Finds the shortest path using breadth first search. Note that
+     * it calcultes the paths to all node, but we only return the path
+     * from the source node to the target node
+     * This does not take edge weights into account. Instead,
+     * it keeps track of the number of edges.
+     * Source(Discussion Slides on Algorithms)
+     * @param start the source node
+     * @param end the target node
+     * @return vector of strings of city-state to get from start to end
+     */
+    vector<string> modifiedBFS(string start, string end);
 };
 
-void Graph::addVertex(string key){
-    //Check if it is in the graph
-    if(adjList.find(key) == adjList.end())
-    {
-        adjList[key] = {};
+//O(number of sightings)
+void Graph::addVertex(const string& city){
+    //Check if sighting is already in the graph because there are multiple sightings in same location
+    if (adjList.find(city) == adjList.end()) {
+        //initialize the cumulative distance of the sighting to the largest number
+        //this is because we will use it in the dijkstra algorithm
+        get<1>(adjList[city]) = { std::numeric_limits<long double>::max() };
+        numVertices++; //add to the total number of vertices
     }
 }
 
-void Graph::addEdge(string start, string end){
-    Edge newEdge;
-    newEdge.target_destination = end;
-    newEdge.weight = calculateEdgeWeight(sighting.getLatLong(start), sighting.getLatLong(end));
-
+//O(number of sightings less than threshold)
+void Graph::addEdge(const string& from, const string& to, long double weight, pair<long double, long double> latLong){
+    Edge newEdge = Edge(to, weight, latLong);
     // Add the new edge to the start city's list of edges
-    adjList[start].push_back(newEdge);
+    get<0>(adjList[from]).push_back(newEdge);
+
 }
 
-void Graph::create(vector<string> v){
-    //Check if it is in the graph
-    for (const auto& a : cityDates)
-    {
-        string cityDate = sighting.getData(city, a) + sighting.getData(dateTime, a);
-        addVertex(cityDate);
+void Graph::create(UFOSightings& sightings) {
+    vector<pair<string, string>> v = sightings.v;
+
+    //one way forward
+    for (unsigned int i  = 0 ;  i < v.size() - 1 ; i ++){
+        auto currentSighting = v[i];
+        //cout << v[i].first << endl;
+        auto destSighting = v[i+1];
+        string currentCity = sightings.m[currentSighting].city + '-' + sightings.m[currentSighting].state ;
+        //   cout << currentCity << endl;
+        addVertex(currentCity);
+
+        string otherCity = sightings.m[destSighting].city + '-' + sightings.m[destSighting].state;
+
+        if (currentCity != otherCity)
+        {
+            long double weight = calculateEdgeWeight(sightings.getLatLong(currentSighting), sightings.getLatLong(destSighting));
+
+            if (weight < threshold)
+                addEdge(currentCity, otherCity, weight, sightings.getLatLong(destSighting));
+
+        }
     }
 
+    //one way backward to make the graph bi-directional
+    for (unsigned int i  = v.size() - 1;  i > 0; i --){
+        auto currentSighting = v[i];
+        //cout << v[i].first << endl;
+        auto destSighting = v[i-1];
+        string currentCity = sightings.m[currentSighting].city + '-' + sightings.m[currentSighting].state ;
+        //   cout << currentCity << endl;
+        addVertex(currentCity);
+
+        string otherCity = sightings.m[destSighting].city + '-' + sightings.m[destSighting].state;
+
+        if (currentCity != otherCity)
+        {
+            long double weight = calculateEdgeWeight(sightings.getLatLong(currentSighting), sightings.getLatLong(destSighting));
+
+            if (weight < threshold)
+                addEdge(currentCity, otherCity, weight, sightings.getLatLong(destSighting));
+
+        }
+    }
+
+
 }
-long double Graph::calculateEdgeWeight(pair<long double,long double> start, pair<long double,long double> end) {
-    long double edgeWeight = 0; //holds the distance in km of the two points
+
+
+
+long double Graph::calculateEdgeWeight(pair<long double, long double> start, pair<long double, long double> end) {
+    long double edgeWeight = 0.0; //holds the distance in km of the two points
+
     //convert longitudes and latitudes to radians
-    long double latitude_start = ((M_PI) / 180 ) * start.first;
-    long double longitude_start = ((M_PI) / 180 ) * start.second;
-    long double latitude_end = ((M_PI) / 180 ) * end.first;
-    long double longitude_end = ((M_PI) / 180 ) * end.second;
+    long double latitude_start = ((X_PI) / 180) * start.first;
+    long double longitude_start = ((X_PI) / 180) * start.second;
+    long double latitude_end = ((X_PI) / 180) * end.first;
+    long double longitude_end = ((X_PI) / 180) * end.second;
 
     //Using the Haversine Formula
-    long double dist_long = abs(longitude_end - longitude_start);
-    long double dist_lat = abs(latitude_end - latitude_start);
-
-    edgeWeight = 2 * asin(sqrt( pow(sin(dist_lat/2),2) + cos(latitude_start) * cos(latitude_end) * pow(sin(dist_long/2), 2)));
+    long double distances_long = abs(longitude_end - longitude_start);
+    long double distances_lat = abs(latitude_end - latitude_start);
+    edgeWeight = 2 * asin(sqrt(pow(sin(distances_lat / 2), 2) + cos(latitude_start) * cos(latitude_end) * pow(sin(distances_long / 2), 2)));
 
     //convert into kilometers using the earth's radius in km is 6371
     edgeWeight = edgeWeight * 6371;
 
     return edgeWeight;
-
 }
+
+//Note that key : vector<Edge>, distance, bool
+vector<string> Graph::dijkstra(const string& start, const string& end)
+{
+    //vector<string> path;
+    priority_queue<pair<long double, string>, vector<pair<long double, string>>, greater<>> pq;
+
+    //initialize contents of the queue and cumulative distance
+    pq.emplace(0.0, start);
+    get<1>(adjList[start]) = 0.0; //distance to itself which would be the start node
+
+    unordered_map<string, string> previous; //keeps track of the visited nodes
+
+    while (!pq.empty())
+    {
+        pair<long double, string> curr = pq.top();
+        string from = curr.second; //city-state of the from node
+        long double currDistance = curr.first; //from to distance
+
+        pq.pop();
+
+        for (const Edge& edge : get<0>(adjList[from])) {
+            string to = edge.to;  //city-state of the neighboring node of from
+            long double distance = edge.weight;
+
+            if (currDistance + distance < get<1>(adjList[to])) {
+                get<1>(adjList[to]) = currDistance + distance; //update the cumulative distance
+                pq.emplace(get<1>(adjList[to]), to);
+                previous[to] = from; //add to the list of visited nodes
+            }
+        }
+    }
+
+    if (previous.find(end) != previous.end()) {  //if the end node is in the dijsktra shortest path
+        string current = end; //set the current node to the ending node
+        vector<string> shortestPath; //holds path from starting node to the ending node
+
+        //starting from the target end node we are going backwards to find path back to source
+        while (current != start) { //while we have not reached the source node
+            shortestPath.push_back(current);
+            current = previous[current];
+        }
+        shortestPath.push_back(start); //add the source node
+
+        //since we were traversing the dijsktra shortest path from the end to the start
+        //we need to reverse it so that it goes from source to target.
+        reverse(shortestPath.begin(), shortestPath.end());
+
+        return shortestPath;
+    }
+    else{   //this means that the source node has no path to target node
+        return {"You can't drive to this UFO sighting :("};
+    }
+}
+
+vector<string> Graph::modifiedBFS(string start, string end){
+    vector<pair<string,bool>> visited;
+    vector<string> path;
+
+    queue<string> q; //to keep track of the current visited nodes
+    q.push(start);
+    visited.push_back(make_pair(start,true));
+    path.push_back(start);
+
+    while(!q.empty()) {
+        string curr = q.front();
+        q.pop();
+
+        for (const Edge& edge : get<0>(adjList[curr])){
+            //check if the edge is in the visited vector
+            auto it = find_if(visited.begin(), visited.end(), [&edge](const pair<string,bool>& p){
+                return p.first == edge.to;
+            });
+
+            if (it == visited.end()) { //if the edge is NOT found
+                path.push_back(edge.to);
+                //update that the visited vector to include that this edge has been visited
+                visited.push_back(make_pair(edge.to,true));
+                q.push(edge.to);
+
+                if(edge.to == end){
+                    return path;
+                }
+            }
+        }
+    }
+    return {"You can't drive to this UFO sighting :("};
+}
+
+
+
+
 
 #endif //P3_GRAPH_H
--- main.cpp
+++ main.cpp
@@ -1,33 +1,181 @@
-#include <iostream>
-#include <UFOSightings.h>
-#include <Graph.h>
+#include "iostream"
+#include "UFOSightings.h"
+#include "Graph.h"
+#include <matplot/matplot.h>
+#include <utility>
+#include <cmath>
+
 using namespace std;
-
-int main() {
-    UFOSightings() sightings;
-	sightings.parseFile("ufo_sighting_data.csv");
-	Graph graph;
-    string start, target; //Keys
-	
-    graph.create(sightings.getCityDates());
-    
-    //UI stuff for input selection
-    cin >> start; //Get start key
-    cin >> target; //Get end key
-    //If we want to input weight, do it here, pass to graph
-
-	startCity = getCity(start)
-    targetCity = getCity(target); //Get city data from keys
-
-    //UI stuff for algo selection
-    Graph.dijkstra(startCity, targetCity);
-    Graph.modifiedBFS(startCity, targetCity);
-
-    //UI stuff to display string
-    for (const string& city : graph.getPath()) {
-        cout << city << " ";
+void output(Graph& graph, UFOSightings& sightings, vector<string> path, int algo) {
+    ofstream fs("./output.txt");
+    if (!fs.is_open()) {
+        cout << "Output file not found" << endl;
+        return;
     }
 
-    return 0;
+    if (algo == 1) {
+        cout << "SHORTEST PATH USING BFS" << endl;
+        fs << "SHORTEST PATH USING BFS" << endl;
+    }
+    if (algo == 2) {
+        cout << "SHORTEST PATH USING DJIKSTRA'S ALGORITHM" << endl;
+        fs << "SHORTEST PATH USING DJIKSTRA'S ALGORITHM" << endl;
+    }
+    cout << endl;
+    fs << endl;
+    for (size_t i = 0; i < path.size(); i++) {
+        cout << path[i];
+        fs << path[i];
+        if (i < path.size() - 1) {
+            cout << " -> ";
+            fs << " -> ";
+        }
+        else {
+            cout << endl;
+            fs << endl;
+            cout << setw(20) << setfill('_') << "" << endl;
+            fs << setw(200) << setfill('_') << "" << endl;
+        }
+    }
 
+    for (auto cityState : path) {
+        for (auto cityDate : sightings.v) {
+            if (cityState == sightings.getData("city", cityDate) + "-" + sightings.getData("state", cityDate)) {
+                cout << sightings.getData("city", cityDate) << ", " << sightings.getData("state", cityDate) << endl << endl;
+                fs << sightings.getData("city", cityDate) << ", " << sightings.getData("state", cityDate) << endl << endl;
+
+                cout << "Date and Time: " << sightings.getData("dateTime", cityDate) << endl;
+                fs << "Date and Time: " << sightings.getData("dateTime", cityDate) << endl;
+
+                cout << "Description: " << sightings.getData("desc", cityDate) << endl;
+                fs << "Description: " << sightings.getData("desc", cityDate) << endl;
+
+                cout << "Length: " << sightings.getData("length", cityDate) << endl;
+                fs << "Length: " << sightings.getData("length", cityDate) << endl;
+
+                cout << "Latitude: " << sightings.getData("lat", cityDate) << endl;
+                fs << "Latitude: " << sightings.getData("lat", cityDate) << endl;
+
+                cout << "Longitude: " << sightings.getData("long", cityDate) << endl;
+                fs << "Longitude: " << sightings.getData("long", cityDate) << endl;
+
+                break;
+            }
+        }
+        cout << setw(20) << setfill('_') << "" << endl;
+        fs << setw(200) << setfill('_') << "" << endl;
+    }
+    fs.close();
 }
+int main() {
+
+    using namespace matplot;
+    Graph graph; //create empty graph
+    cout << "Graph container created successfully" << endl;
+    UFOSightings sightings; //create container for sightings
+    cout << "Sighting container created successfully" << endl;
+
+    //Fill the graph with sightings
+    sightings.parseFile("./ufo_sighting_data.csv", 80332);
+    cout << "Parse file and sighting objects created successfully" << endl;
+    graph.create(sightings);
+    cout << "Graph created successfully" << endl;
+
+    //Ask user for the starting and ending location and for the type of path
+    string start = "";
+    string end = "";
+    int path_type = 0;
+
+    cout << "Enter the city-state (eg. houston-tx) of the starting UFO sighting" << endl;
+    getline(cin, start);
+    cout << "Enter the city-state (eg. white plains-ny) of the target/destination UFO sighting" << endl;
+    getline(cin, end);
+    cout << "Would you like to take the scenic route? Type 1 for yes, 2 for no" << endl;
+    cin >> path_type;
+
+    //BFS
+    if (path_type == 1)
+    {
+        vector<string> bfs = graph.modifiedBFS(start, end);
+        vector<pair<long double, long double>> coords;
+        output(graph, sightings, bfs, path_type);
+
+        for(auto location: bfs)
+        {
+            size_t pos = location.find('-');
+            string city = location.substr(0, pos);
+            string state = location.substr(pos + 1);
+            pair<string, string> key = make_pair(city, state);
+
+            coords.push_back(sightings.getLatLong(key));
+        }
+
+        // Source(https://alandefreitas.github.io/matplotplusplus/plot-types/geography/geoplot/)
+        for(unsigned int i = 0; i < coords.size() - 1; i++) {
+            if (i == 0 || i == coords.size() - 2)
+                geoplot(vector{coords[i].first, coords[i + 1].first}, vector{coords[i].second, coords[i + 1].second},
+                        "g-*");
+
+            else
+                geoplot(vector{coords[i].first, coords[i + 1].first}, vector{coords[i].second, coords[i + 1].second},
+                        "g-*");
+            hold(on);
+
+            geolimits(13.8183, 49.4595, -135.207, -44.8848);
+            show();
+
+        }
+    }
+
+    //Dijsktra
+    if (path_type == 2)
+    {
+        vector<string> djkstr = graph.dijkstra(start, end);
+        vector<pair<long double, long double>> coords;
+
+        output(graph, sightings, djkstr, path_type);
+
+
+        for(auto location: djkstr)
+        {
+            size_t pos = location.find('-');
+            string city = location.substr(0, pos);
+            string state = location.substr(pos + 1);
+            pair<string, string> key = make_pair(city, state);
+
+            coords.push_back(sightings.getLatLong(key));
+        }
+
+        // Source (https://alandefreitas.github.io/matplotplusplus/plot-types/geography/geoplot/)
+        for(unsigned int i = 0; i < coords.size() - 1; i++) {
+            if (i == 0 || i == coords.size() - 2)
+                geoplot(vector{coords[i].first, coords[i + 1].first}, vector{coords[i].second, coords[i + 1].second},
+                        "g-*");
+
+            else
+                geoplot(vector{coords[i].first, coords[i + 1].first}, vector{coords[i].second, coords[i + 1].second},
+                        "g-*");
+            hold(on);
+
+            geolimits(13.8183, 49.4595, -135.207, -44.8848);
+            show();
+
+        }
+
+    }
+}
+
+
+//testing the graph and sighitings object
+//if (graph.adjList.find("houston") != graph.adjList.end()) cout << "Found";
+//cout << graph.calculateEdgeWeight(make_pair(29.7630556, -95.3630556), make_pair(41.523427, -73.646795));
+// cout << graph.calculateEdgeWeight(make_pair(29.7630556, -95.3630556), make_pair(41.523427, -73.646795));
+// for (pair<string, string>& key : sightings.v) {
+/*cout << "City: " << sightings.m[key].city << endl;
+cout << "Date/Time: " << sightings.m[key].date_time << endl;
+cout << "Length: " << sightings.m[key].length << endl;
+cout << "Desc: " << sightings.m[key].desc << endl;
+cout << "Coords: " << sightings.m[key].coords.first << ", " << sightings.m[key].coords.first << endl;*/
+//}
+//Vsit this UFO sighting
+//go here 4/29/2005 21:00,cary,nc,us,light,90,90 sec,((NUFORC Note:  Missile launch at 2055 hrs (EDT).  PD))  Two small yellow-white lights moving parallel to horizon; fuzzy ring of light,5/11/2005,35.7913889,-78.7813889
